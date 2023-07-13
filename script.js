358029fcfd7f16134caf9b2d2c2efd6afd232364
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-07-12 21:58:54 -0400

    Establish ui elements in script

---
+++ script.js
@@ -0,0 +1,16 @@
+const ui = {
+    buttons: {
+        digits: document.querySelectorAll('.button.digit'),
+        operations: document.querySelectorAll('.button.operation'),
+        negative: document.querySelector('#negative-button'),
+        decimal: document.querySelector('#decimal-button'),
+        clear: document.querySelector('#clear-button'),
+        backspace: document.querySelector('#backspace-button'),
+        equals: document.querySelector('#equals-button'),
+    },
+    displays: {
+        main: document.querySelector('#main-display'),
+        sign: document.querySelector('#sign-display'),
+    },
+}
+
