04efa2e64c32bb42b09a48abc8d0100e3483fad9
Ben <121274141+bdaniels8135@users.noreply.github.com>
2023-07-12 22:25:21 -0400

    Add methods to UI object for updating displays

--- script.js
+++ script.js
@@ -11,6 +11,28 @@ const ui = {
     displays: {
         main: document.querySelector('#main-display'),
         sign: document.querySelector('#sign-display'),
+        
+        updateMain(newText) {
+            this.main.textContent = newText;
+        },
+
+        toggleSign() {
+            if (this.sign.innerHTML) {
+                this.sign.innerHTML = '';
+            } else {
+                this.sign.innerHTML = '&minus;';
+            }
+        }
     },
+};
+
+function addButtonClickListeners() {
+    for (button of ui.buttons.digits) {
+        button.addEventListener('click', () => resolveDigitClick(Number(button.textContent)));
+    };
+}
+
+function resolveDigitClick(digit) {
+    console.log(digit)
 }
 
